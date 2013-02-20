31de311adc9b7180b5f1bbed79b4dcfd4b104875
Tommy Su <dscvr.software@gmail.com>
2013-02-19 23:20:07 -0800

    Initial commit

---
+++ Projects/Discvr_Rails/DiscvrApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscvrApp::Application.config.secret_token = '1b44ee2e10584a99de7ac70ea98cabb58698aa780a8a9e0638e1407f3bdcfd862141cc20a1b6936ccdf1ffa5f1ed119679d524a0f21ca134457cbe2f6db9f9de'
